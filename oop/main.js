f818336c15e6de45e356a9c131245290367b0fe7
Kate Park <rudhi1009@gmail.com>
2019-04-12 15:55:42 -0700

    guessing game to OOP

--- oop/main.js
+++ oop/main.js
@@ -1,7 +1,10 @@
 
-
 $(document).ready(startApp);
-
+var guessGame;   
+var randomNumber;
 function startApp(){
-	
+	guessGame = new Guess();
+	guessGame.guess_start();
+	// randomNumber = new RandomGenerator( min, max);	
 }
+
